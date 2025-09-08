41f7c34aca446b048f74e4aa34a9991056d2f657
bibindon <bibindon0814@gmail.com>
2025-09-08 10:19:15 +0900

    s

--- simple-directx9/main.cpp
+++ simple-directx9/main.cpp
@@ -49,6 +49,11 @@ static void Cleanup();
 static void Render();
 LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
+extern int WINAPI _tWinMain(_In_ HINSTANCE hInstance,
+                            _In_opt_ HINSTANCE hPrevInstance,
+                            _In_ LPTSTR lpCmdLine,
+                            _In_ int nCmdShow);
+
 int WINAPI _tWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPTSTR lpCmdLine,
